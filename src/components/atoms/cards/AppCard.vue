bf36b47681683aa4082aa6cac308f7b53d48f5eb
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2023-11-19 21:48:55 -0800

    faet: add appCard & homeStudent

---
+++ src/components/atoms/cards/AppCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  infos: any
+}>()
+</script>
+
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    :prepend-avatar="infos.imagem"
+    variant="outlined"
+  >
+    <v-card-item>
+      <div>
+        <div class="text-h6 mb-1">
+          {{ infos.titulo }}
+        </div>
+        <div class="text-caption">{{ infos.descricao }}</div>
+      </div>
+    </v-card-item>
+
+    <v-card-actions class="ml-2">
+      <v-btn rounded>
+        {{ infos.qtdModulos }} Módulos
+      </v-btn>
+      <v-btn rounded>
+        {{ infos.qtdAulas }} Aulas
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
---
+++ src/views/HomeStudent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppCard from "@/components/atoms/cards/AppCard.vue";
+import {ref} from "vue";
+
+const infos = ref({
+  imagem: "https://cdn.vuetifyjs.com/images/john.jpg",
+  titulo: "Docker",
+  descricao: "Você vai aprender como criar, administrar ambientes isolados através de containers com o Docker.",
+  qtdModulos: 4,
+  qtdAulas: 43
+})
+</script>
+
+<template>
+  <v-container class="pa-15">
+    <h2>Cursos</h2>
+
+    <AppCard :infos="infos"></AppCard>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
